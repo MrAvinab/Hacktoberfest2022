1cbb242bc6075296b2d92135a84282ad03180c94
AVINAB <111741648+MrAvinab@users.noreply.github.com>
2022-10-14 18:24:08 +0530

    Program to display the Fibonacci sequence

--- C++.cpp
+++
@@ -1,7 +0,0 @@
-//What is an abstraction in C++?
-
-
-// Abstraction means displaying the essential details to the user while hiding the irrelevant or particular details that you don’t want to show to a user. It is of two types:
-
-// Control abstraction
-// Data abstraction
